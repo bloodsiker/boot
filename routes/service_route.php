03ecd5269158aece2d7cf099aa1305d7ddf0166e
bloodsiker <maldini2@ukr.net>
2017-05-13 12:08:12 +0000

    edit routes

--- routes/service_route.php
+++ routes/service_route.php
@@ -1,9 +1,18 @@
 <?php
 
 Route::group(['middleware' => ['service.center']], function (){
+    View::composer('service_center_cabinet.layouts.sidebar', function($view)
+    {
+        $service_centers = Auth::user()->service_centers;
+        $view->with(['service_centers' => $service_centers]);
+    });
     Route::get('cabinet', 'ServiceCenterCabinet\CabinetController@getIndex')->name('cabinet');
     Route::get('cabinet/dashboard', 'ServiceCenterCabinet\CabinetController@getDashboard')->name('cabinet.dashboard');
     Route::get('cabinet/sc/{id}', 'ServiceCenterCabinet\CabinetController@getService')->name('cabinet.service')->where('id', '[0-9]+');
+    Route::put ('cabinet/sc/{id}/update', 'ServiceCenterCabinet\CabinetController@putUpdateService')->name('cabinet.update.service')->where('id', '[0-9]+');
+    Route::post ('cabinet/sc/{id}/add-personal', 'ServiceCenterCabinet\CabinetController@postAddPersonalService')->name('cabinet.add.personal.service')->where('id', '[0-9]+');
+
     Route::get('cabinet/add/service', 'ServiceCenterCabinet\CabinetController@getAddService')->name('cabinet.add.service');
+    Route::post('cabinet/add/service', 'ServiceCenterCabinet\CabinetController@postAddService')->name('cabinet.add.service');
     Route::get('cabinet/logout', 'ServiceCenterCabinet\CabinetController@getLogout')->name('cabinet.logout');
 });
