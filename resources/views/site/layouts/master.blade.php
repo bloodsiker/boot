8ba90f8057f9cb3d9f76270c17b66ba8ece48f12
bloodsiker <maldini2@ukr.net>
2017-05-29 10:03:15 +0000

    add meta name=_token

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -7,6 +7,7 @@
     <title>@yield('title')</title>
     <meta name="description" content="@yield('description')">
     <meta name="keywords" content="@yield('keywords')">
+    <meta name="_token"  content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('site/vendor/bootstrap/dist/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('site/vendor/angular/angular-csp.css') }}">
