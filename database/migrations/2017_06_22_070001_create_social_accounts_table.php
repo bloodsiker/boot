b173662435197e80c315030da049a69e90644848
bloodsiker <maldini2@ukr.net>
2017-07-14 14:43:44 +0000

    fix migration

--- database/migrations/2017_06_22_070001_create_social_accounts_table.php
+++ database/migrations/2017_06_22_070001_create_social_accounts_table.php
@@ -15,10 +15,15 @@ class CreateSocialAccountsTable extends Migration
     {
         Schema::create('social_accounts', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
             $table->string('provider_user_id');
             $table->string('provider');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
     }
 
