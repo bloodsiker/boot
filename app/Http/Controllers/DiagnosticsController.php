57803ac8bc6fec31745464e943b9c34910f8fb2a
Dima Ovsijchuk <maldini2@ukr.net>
2017-10-24 08:23:43 +0000

    fix diagnostic

--- app/Http/Controllers/DiagnosticsController.php
+++ app/Http/Controllers/DiagnosticsController.php
@@ -50,94 +50,150 @@ class DiagnosticsController extends Controller
                 ->get()
                 ->toArray();
             $result['problem_watching'] = array_column($problem_watching, 'problem_watching');
-            return $result;
-        }
 
-        // выбран "знаю точно", показываем "наблюдаю"
-        if($action == 'problem_know'){
-            $type_device = $request->type_device;
-            $problem_know = $request->problem_know;
-            $result = Diagnostic::select(['problem_watching'])
+            $problem_description = Diagnostic::select(['problem_description'])
                 ->distinct()
                 ->where('type_device', $type_device)
-                ->where('problem_know', $problem_know)
-                ->orderBy('problem_watching')
-                ->get()
-                ->toArray();
-            $result = array_column($result, 'problem_watching');
-            return $result;
-        }
-
-        //выбран "наблюдаю", показываем "знаю точно"
-        if($action == 'problem_watching'){
-            $type_device = $request->type_device;
-            $problem_watching = $request->problem_watching;
-            $result = Diagnostic::select(['problem_know'])
-                ->distinct()
-                ->where('type_device', $type_device)
-                ->where('problem_watching', $problem_watching)
-                ->orderBy('problem_know')
-                ->get()
-                ->toArray();
-            $result = array_column($result, 'problem_know');
-            return $result;
-        }
-
-        // выбран и "знаю точно" и "наблюдаю", показываем "описание дефекта"
-        if($action == 'problem_know_watching'){
-            $type_device = $request->type_device;
-            $problem_know = $request->problem_know;
-            $problem_watching = $request->problem_watching;
-            $result = Diagnostic::select(['problem_description'])
-                ->distinct()
-                ->where('type_device', $type_device)
-                ->where('problem_know', $problem_know)
-                ->where('problem_watching', $problem_watching)
                 ->orderBy('problem_description')
                 ->get()
                 ->toArray();
-            $result = array_column($result, 'problem_description');
+            $result['problem_description'] = array_column($problem_description, 'problem_description');
             return $result;
         }
 
-        // показываем результатирущую таблицу диагностики
-        if($action == 'problem_description'){
+        /**********************************************/
+
+        if($action == 'problem_know' || $action == 'problem_watching' || $action == 'problem_description'){
             $type_device = $request->type_device;
             $problem_know = $request->problem_know;
             $problem_watching = $request->problem_watching;
             $problem_description = $request->problem_description;
-            $result = Diagnostic::select(['spare_part', 'percentage', 'services'])
-                ->where('type_device', $type_device)
-                ->where('problem_know', $problem_know)
-                ->where('problem_watching', $problem_watching)
-                ->where('problem_description', $problem_description)
-                ->orderBy('spare_part')
-                ->get()
-                ->toArray();
 
-            $list_result = [];
-            $i = 0;
-            foreach ($result as $value){
-                $list_result[$i]['percentage'] = $value['percentage'];
-                $list_result[$i]['services'] = $value['services'];
-                $list_result[$i]['spare_part'] = $value['spare_part'];
-                $list_result[$i]['min_price'] = DB::table('service_center_price')->where('title', $value['services'])->min('price_min');
-                $list_result[$i]['max_price'] = DB::table('service_center_price')->where('title', $value['services'])->max('price_max');
-                $i++;
+            if($problem_know !== false){
+                $result_know = Diagnostic::select(['problem_know', 'problem_watching', 'problem_description'])
+                    ->distinct()
+                    ->where('type_device', $type_device)
+                    ->where('problem_know', $problem_know)
+                    ->orderBy('problem_know')
+                    ->get()
+                    ->toArray();
+                $result['problem_watching'] = array_unique(array_column($result_know, 'problem_watching'));
+                $result['problem_description'] = array_unique(array_column($result_know, 'problem_description'));
             }
 
-            // Пишем статистику просмотров диагностики
-            DiagnosticRequest::create([
-                'type_device' => $request->type_device,
-                'problem_know' => $request->problem_know,
-                'problem_watching' => $request->problem_watching,
-                'problem_description' => $request->problem_description,
-                'ip_address' => \Request::ip(),
-                'created_at' => Carbon::now()
-            ]);
+            if($problem_watching !== false){
+                $result_watching = Diagnostic::select(['problem_know', 'problem_watching', 'problem_description'])
+                    ->distinct()
+                    ->where('type_device', $type_device)
+                    ->where('problem_watching',  $problem_watching)
+                    ->orderBy('problem_watching')
+                    ->get()
+                    ->toArray();
+                $result['problem_know'] = array_unique(array_column($result_watching, 'problem_know'));
+                $result['problem_description'] = array_unique(array_column($result_watching, 'problem_description'));
+            }
 
-            return $list_result;
+            if($problem_description !== false){
+                $result_description = Diagnostic::select(['problem_know', 'problem_watching', 'problem_description'])
+                    ->distinct()
+                    ->where('type_device', $type_device)
+                    ->where('problem_description', $problem_description)
+                    ->orderBy('spare_part')
+                    ->get()
+                    ->toArray();
+                $result['problem_know'] = array_unique(array_column($result_description, 'problem_know'));
+                $result['problem_watching'] = array_unique(array_column($result_description, 'problem_watching'));
+            }
+            return $result;
         }
+
+        /**********************************************/
+
+        // выбран "знаю точно", показываем "наблюдаю"
+//        if($action == 'problem_know'){
+//            $type_device = $request->type_device;
+//            $problem_know = $request->problem_know;
+//            $result = Diagnostic::select(['problem_watching'])
+//                ->distinct()
+//                ->where('type_device', $type_device)
+//                ->where('problem_know',  $problem_know)
+//                ->orderBy('problem_watching')
+//                ->get()
+//                ->toArray();
+//            $result = array_column($result, 'problem_watching');
+//            return $result;
+//        }
+//
+//        //выбран "наблюдаю", показываем "знаю точно"
+//        if($action == 'problem_watching'){
+//            $type_device = $request->type_device;
+//            $problem_watching = $request->problem_watching;
+//            $result = Diagnostic::select(['problem_know'])
+//                ->distinct()
+//                ->where('type_device', $type_device)
+//                ->where('problem_watching', $problem_watching)
+//                ->orderBy('problem_know')
+//                ->get()
+//                ->toArray();
+//            $result = array_column($result, 'problem_know');
+//            return $result;
+//        }
+//
+//        // выбран и "знаю точно" и "наблюдаю", показываем "описание дефекта"
+//        if($action == 'problem_know_watching'){
+//            $type_device = $request->type_device;
+//            $problem_know = $request->problem_know;
+//            $problem_watching = $request->problem_watching;
+//            $result = Diagnostic::select(['problem_description'])
+//                ->distinct()
+//                ->where('type_device', $type_device)
+//                ->where('problem_know', $problem_know)
+//                ->where('problem_watching', $problem_watching)
+//                ->orderBy('problem_description')
+//                ->get()
+//                ->toArray();
+//            $result = array_column($result, 'problem_description');
+//            return $result;
+//        }
+//
+//        // показываем результатирущую таблицу диагностики
+//        if($action == 'problem_description'){
+//            $type_device = $request->type_device;
+//            $problem_know = $request->problem_know;
+//            $problem_watching = $request->problem_watching;
+//            $problem_description = $request->problem_description;
+//            $result = Diagnostic::select(['spare_part', 'percentage', 'services'])
+//                ->where('type_device', $type_device)
+//                ->where('problem_know', $problem_know)
+//                ->where('problem_watching', $problem_watching)
+//                ->where('problem_description', $problem_description)
+//                ->orderBy('spare_part')
+//                ->get()
+//                ->toArray();
+//
+//            $list_result = [];
+//            $i = 0;
+//            foreach ($result as $value){
+//                $list_result[$i]['percentage'] = $value['percentage'];
+//                $list_result[$i]['services'] = $value['services'];
+//                $list_result[$i]['spare_part'] = $value['spare_part'];
+//                $list_result[$i]['min_price'] = DB::table('service_center_price')->where('title', $value['services'])->min('price_min');
+//                $list_result[$i]['max_price'] = DB::table('service_center_price')->where('title', $value['services'])->max('price_max');
+//                $i++;
+//            }
+//
+//            // Пишем статистику просмотров диагностики
+//            DiagnosticRequest::create([
+//                'type_device' => $request->type_device,
+//                'problem_know' => $request->problem_know,
+//                'problem_watching' => $request->problem_watching,
+//                'problem_description' => $request->problem_description,
+//                'ip_address' => \Request::ip(),
+//                'created_at' => Carbon::now()
+//            ]);
+//
+//            return $list_result;
+//        }
 
         //Перенаправляем на страницу каталога с фильтрацией по данным услугам
         if($action == 'pick_up_service'){
